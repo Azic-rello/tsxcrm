a8cae936223c83c5f7176bd875c5bf69e6ce33db
Azic-rello <github1akk2009@gmail.com>
2026-02-26 21:57:34 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
         <Route path="/admin/envrolment" element={<Envrolment />} />
         <Route path="/admin/assigment" element={<Assigment />} />
       </Route>
+      {/* lhlhk */}
     </Routes>
   );
 }
